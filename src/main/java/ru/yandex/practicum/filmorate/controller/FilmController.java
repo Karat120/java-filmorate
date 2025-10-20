fd6977ee800ffa96ff205bca60a373fe5354def2
Marchinskiy Evgeniy <marchinskiye22@mail.ru>
2025-10-20 16:59:01 +0700

    Add FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,43 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    private final Map<Long, Film> films = new HashMap<>();
+    private long lastGeneratedID = 0;
+
+    @PostMapping
+    public Film createFilm(@RequestBody Film film) {
+        film.setId(lastGeneratedID++);
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @GetMapping
+    public List<Film> getAllFilms() {
+        return new ArrayList<>(films.values());
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film film) {
+        if (!films.containsKey(film.getId())) {
+            throw new FilmNotFoundException();
+        }
+        films.put(film.getId(), film);
+        return film;
+    }
 }
