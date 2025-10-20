4e8db360163339ff27b6adbec9105e9efbcb8d8a
Marchinskiy Evgeniy <marchinskiye22@mail.ru>
2025-10-20 17:05:45 +0700

    Add UserController

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,43 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final Map<Long, User> users = new HashMap<>();
+    private long lastGeneratedID = 0;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        user.setId(++lastGeneratedID);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return new ArrayList<>(users.values());
+    }
+
+    @PutMapping
+    public User updateUser(@RequestBody User user) {
+        if (!users.containsKey(user.getId())) {
+            throw new UserNotFoundException();
+        }
+        users.put(user.getId(), user);
+        return user;
+    }
+}
