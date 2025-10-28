a2928989a5b490c4f7ad4d4250d4853d564e170d
Marchinskiy Evgeniy <marchinskiye22@mail.ru>
2025-10-28 22:44:04 +0700

    Fix SequentialIdGenerator
    Add scope prototype annotation

--- src/main/java/ru/yandex/practicum/filmorate/util/SequentialIdGenerator.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/SequentialIdGenerator.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.filmorate.util;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class SequentialIdGenerator implements IdGenerator<Long> {
     private Long lastGeneratedID = 0L;
-    
+
     public Long generate() {
         return ++lastGeneratedID;
     }
