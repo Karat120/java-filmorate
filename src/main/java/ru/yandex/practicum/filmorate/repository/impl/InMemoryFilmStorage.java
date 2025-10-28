65c9804e4596acebe29b661a7e71c06e97f52a2e
Marchinskiy Evgeniy <marchinskiye22@mail.ru>
2025-10-28 22:53:41 +0700

    Add InMemoryFilmStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/repository/impl/InMemoryFilmStorage.java
@@ -0,0 +1,55 @@
+package ru.yandex.practicum.filmorate.repository.impl;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.repository.FilmStorage;
+import ru.yandex.practicum.filmorate.util.IdGenerator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Component
+@AllArgsConstructor
+public class InMemoryFilmStorage implements FilmStorage {
+    private final Map<Long, Film> films = new HashMap<>();
+    private final IdGenerator<Long> idGenerator;
+
+    @Override
+    public Film addFilm(Film film) {
+        film.setId(idGenerator.generate());
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @Override
+    public Optional<Film> getFilmById(Long id) {
+        return Optional.ofNullable(films.get(id));
+    }
+
+    @Override
+    public List<Film> getAllFilms() {
+        return new ArrayList<>(films.values());
+    }
+
+    @Override
+    public Film updateFilm(Film film) {
+        if (film.getId() == null || !films.containsKey(film.getId())) {
+            throw new UserNotFoundException();
+        }
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @Override
+    public void deleteFilm(Long id) {
+        if (!films.containsKey(id)) {
+            throw new UserNotFoundException();
+        }
+        films.remove(id);
+    }
+}
