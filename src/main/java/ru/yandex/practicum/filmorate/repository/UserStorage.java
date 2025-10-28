a791903066f9a9a7229decb1b2160ebfaa4874ba
Marchinskiy Evgeniy <marchinskiye22@mail.ru>
2025-10-28 22:11:25 +0700

    Add UserStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/repository/UserStorage.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.repository;
+
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserStorage {
+    void addUser(User user);
+    Optional<User> getUserById(Long id);
+    List<User> getAllUsers();
+    void updateUser(User user);
+    void deleteUser(Long id);
+}
