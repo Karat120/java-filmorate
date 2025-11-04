9a5c6c49b966a8b64e22b8f6f6bdcfae6c4acd5d
Marchinskiy Evgeniy <marchinskiye22@mail.ru>
2025-11-04 21:40:17 +0700

    Update FilmController
    Use FilmService

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,32 +1,32 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping("/films")
+@AllArgsConstructor
 public class FilmController {
-    private final Map<Long, Film> films = new HashMap<>();
-    private long lastGeneratedID = 0;
+
+    private final FilmService filmService;
 
     @PostMapping
     public Film createFilm(@Valid @RequestBody Film film) {
-        film.setId(++lastGeneratedID);
-        films.put(film.getId(), film);
+        filmService.create(film);
 
         log.info("Film created: id={}, name={}", film.getId(), film.getName());
         return film;
@@ -34,18 +34,24 @@ public class FilmController {
 
     @GetMapping
     public List<Film> getAllFilms() {
+        var films = filmService.getAll();
+
         log.debug("Retrieving all films (total={})", films.size());
-        return new ArrayList<>(films.values());
+        return films;
     }
 
     @PutMapping
     public Film updateFilm(@Valid @RequestBody Film film) {
-        if (!films.containsKey(film.getId())) {
-            log.warn("Attempt to update non-existent film id={}", film.getId());
-            throw new FilmNotFoundException();
-        }
-        films.put(film.getId(), film);
+        filmService.update(film);
+
         log.info("Film updated: id={}, name={}", film.getId(), film.getName());
         return film;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFilm(@PathVariable Long id) {
+        filmService.deleteById(id);
+
+        log.info("Film deleted: id={}", id);
     }
 }
