255a02342d883d3a86adfb2148b03867ac1d1b76
Marchinskiy Evgeniy <marchinskiye22@mail.ru>
2025-11-04 20:24:24 +0700

    Update UserController
    Use UserService

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,25 +9,21 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping("/users")
+@AllArgsConstructor
 public class UserController {
-    private final Map<Long, User> users = new HashMap<>();
-    private long lastGeneratedID = 0;
+    private final UserService userService;
 
     @PostMapping
     public User createUser(@Valid @RequestBody User user) {
-        user.setId(++lastGeneratedID);
-        users.put(user.getId(), user);
+        userService.createUser(user);
 
         log.info("User created: id={}, email={}, login={}", user.getId(), user.getEmail(), user.getLogin());
         return user;
@@ -34,17 +31,16 @@ public class UserController {
 
     @GetMapping
     public List<User> getAllUsers() {
+        var users = userService.getAllUsers();
+
         log.debug("Retrieving all users (total={})", users.size());
-        return new ArrayList<>(users.values());
+        return users;
     }
 
     @PutMapping
     public User updateUser(@Valid @RequestBody User user) {
-        if (!users.containsKey(user.getId())) {
-            log.warn("Attempt to update non-existent user id={}", user.getId());
-            throw new UserNotFoundException();
-        }
-        users.put(user.getId(), user);
+        userService.updateUser(user);
+
         log.info("User updated: id={}, email={}, login={}", user.getId(), user.getEmail(), user.getLogin());
         return user;
     }
