3a0d31c2d233092bf6d23db3d2db2de01b357f13
Marchinskiy Evgeniy <marchinskiye22@mail.ru>
2025-11-10 02:07:29 +0700

    Add UserMapper

---
+++ src/main/java/ru/yandex/practicum/filmorate/repository/mappers/UserMapper.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.filmorate.repository.mappers;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class UserMapper implements RowMapper<User> {
+
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        User user = new User();
+        user.setId(rs.getLong("id"));
+        user.setEmail(rs.getString("email"));
+        user.setLogin(rs.getString("login"));
+        user.setName(rs.getString("name"));
+
+        java.sql.Date birthday = rs.getDate("birthday");
+        if (birthday != null) {
+            user.setBirthday(birthday.toLocalDate());
+        }
+
+        user.setFriends(new HashSet<>());
+        return user;
+    }
+
+    public static void loadFriends(User user, Set<Long> friendIds) {
+        user.setFriends(friendIds);
+    }
+}
