ef5b3b1ed89b26a309a75d96a198a508500fc960
Marchinskiy Evgeniy <marchinskiye22@mail.ru>
2025-11-10 17:39:05 +0700

    Update MpaRating

--- src/main/java/ru/yandex/practicum/filmorate/model/MpaRating.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/MpaRating.java
@@ -3,7 +3,27 @@ package ru.yandex.practicum.filmorate.model;
 public enum MpaRating {
     G,
     PG,
-    PG_13,
+    PG_13("PG-13"),
     R,
-    NC_17
+    NC_17("NC-17");
+    private final String dbValue;
+
+    MpaRating(String dbValue) {
+        this.dbValue = dbValue;
+    }
+
+    MpaRating() {
+        this.dbValue = this.name();
+    }
+
+    public String getDbValue() {
+        return dbValue;
+    }
+
+    public static MpaRating fromDbValue(String value) {
+        for (MpaRating rating : values()) {
+            if (rating.dbValue.equals(value)) return rating;
+        }
+        throw new IllegalArgumentException("Unknown MPA rating: " + value);
+    }
 }
